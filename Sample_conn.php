7b03c94faace3150c630b97f74c8df89fc55348b
meeramca <94525942+meeramca@users.noreply.github.com>
2022-03-04 15:07:17 +0530

    Add files via upload

---
+++ Sample_conn.php
@@ -0,0 +1,20 @@
+<html>
+   <head>
+      <title>Connecting MySQL Server</title>
+   </head>
+   <body>
+      <?php
+         $dbhost = 'localhost';
+         $dbuser = '21mca045';
+         $dbpass = '2699';
+         $mysqli = new mysqli($dbhost, $dbuser, $dbpass);
+         
+         if($mysqli->connect_errno ) {
+            printf("Connect failed: %s<br />", $mysqli->connect_error);
+            exit();
+         }
+         printf('Connected successfully.<br />');
+         $mysqli->close();
+      ?>
+   </body>
+</html>
